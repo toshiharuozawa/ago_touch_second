3a4a9041178883b8ed4fd20a8726575bef150a95
toshiharu.ozawa <hotoke29@gmail.com>
2017-11-11 14:33:11 +0900

    first commit 20171111

---
+++ app/src/main/java/collection/qzawa/com/ago_touch_second/MainActivity.java
@@ -0,0 +1,28 @@
+package collection.qzawa.com.ago_touch_second;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{  //クリックリスナーを実装
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);  //リスナーをボタンに登録
+
+    }
+
+    //ボタンが押された時の処理
+    public void onClick(View view){
+        //ここに遷移するための処理を追加する
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/collection/qzawa/com/ago_touch_second/SecondActivity.java
@@ -0,0 +1,97 @@
+package collection.qzawa.com.ago_touch_second;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private int mCount;
+    private AudioAttributes audioAttributes;
+    private SoundPool soundPool;
+    private int soundOne, soundTwo;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //admobの実装
+        MobileAds.initialize(getApplicationContext(),"ca-app-pub-3940256099942544~3347511713");
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        Intent intent = getIntent();
+
+        audioAttributes = new AudioAttributes.Builder()
+                // USAGE_MEDIA
+                // USAGE_GAME
+                .setUsage(AudioAttributes.USAGE_GAME)
+                // CONTENT_TYPE_MUSIC
+                // CONTENT_TYPE_SPEECH, etc.
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setAudioAttributes(audioAttributes)
+                // ストリーム数に応じて
+                .setMaxStreams(2)
+                .build();
+
+        // one.wav をロードしておく
+        soundOne = soundPool.load(this, R.raw.kiss1, 1);
+        soundTwo = soundPool.load(this, R.raw.kiss2, 1);
+
+
+        // load が終わったか確認する場合
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Log.d("debug","sampleId="+sampleId);
+                Log.d("debug","status="+status);
+            }
+        });
+
+    }
+
+    public void onBackButtonClick(View view) {  // （3）
+        finish();  // （4）
+    }
+
+    private void updateCountView() {
+        TextView countView = (TextView) findViewById(R.id.textView1);
+        countView.setText(String.valueOf(mCount));
+    }
+
+    public void plus(View view) {
+
+        mCount++;
+        updateCountView();
+        if (mCount%5 == 0) {
+            soundPool.play(soundTwo, 1.0f, 1.0f, 0, 0, 1);
+        }else
+            soundPool.play(soundOne, 1.0f, 1.0f, 0, 0, 1);
+    }
+
+    public void clear(View view) {
+
+        mCount = 0;
+        updateCountView();
+
+    }
+
+
+}
+
